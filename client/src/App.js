b356abeee842e81fbc629c8cd205c06e6e99942b
Pegasoos <definitelyisntme@gmail.com>
2022-04-29 20:41:30 -0400

    Logo and Banner Component Complete

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div>
       <Switchboard />
       <Navbar />
+      <Banner />
     </div>
   );
 }
---
+++ client/src/components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner(){
+    return(
+        <div>
+        <div className = "banner-img">
+        </div>
+        <h2>
+        Search For and Save Books of Interest
+        </h2>
+        </div>
+    )
+}
+
+export default Banner;

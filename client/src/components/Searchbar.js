50123e9a35547661b132b671979ab99a6c835dff
Pegasoos <definitelyisntme@gmail.com>
2022-05-01 18:43:39 -0400

    Google Books API Call Function Functional

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -3,10 +3,18 @@ import { Input } from 'antd';
 import './Searchbar.css';
 const { Search } = Input;
 
+const apiKey = 'AIzaSyBDhCTo7MiQy0IutlvFi3BE6mHqJ3rbuVE';
+
 function Searchbar(){
+
+   const bookSearch = (query) =>{
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}+intitle`)
+        .then( response => console.log(response.json()))
+   }
+
     return(
         <div className='search-div'>
-            <Search className = "search-bar" placeholder = "Search For A Book!" />
+            <Search className = "search-bar" placeholder = "Search For A Book!" onSearch={bookSearch}/>
         </div>
     )
 }
